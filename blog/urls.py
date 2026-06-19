d0dc1f9ff041f5b6855b40388f161a2d0d0dc2a6
mohamad-karimi <karimi.mohamad0011@gmail.com>
2026-06-19 16:50:21 +0330

    Static
    Load static in all templates and change the links script and images and extends
    the base.html to the templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blog.views import blog_grid, blog_detail
 urlpatterns = [
-    path('grid/', blog_grid, name="blog_grid"),
+    path('', blog_grid, name="blog_grid"),
     path('detail/', blog_detail, name="blog_detail"),
 ]
