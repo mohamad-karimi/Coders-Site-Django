3e98f6e7d73d1293cc6bc8e84485610ddda84fa3
mohamad-karimi <karimi.mohamad0011@gmail.com>
2026-06-20 11:28:44 +0330

    Dynamic url in base.html
    Change static url to dynamin url in the base.html.

--- instructor/urls.py
+++ instructor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from instructor.views import IN_list, IN_single
 
+app_name = "instructor"
 urlpatterns = [
     path('list/', IN_list, name="instructor_list"),
     path('single/', IN_single, name="instructor_single"),
