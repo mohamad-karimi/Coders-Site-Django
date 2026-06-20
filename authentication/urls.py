7ad9a8dbb14fb164fe214c1b8918add22e0c2f31
mohamad-karimi <karimi.mohamad0011@gmail.com>
2026-06-20 14:40:37 +0330

    Dynamic url in other apps
    Add basic necessary Dynamic url in other apps.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from authentication.views import sign_in, sign_up, forgot_password
 
+app_name="authentication"
+
 urlpatterns = [
     path('login/', sign_in, name='login'),
     path('signup/', sign_up, name='signup'),
